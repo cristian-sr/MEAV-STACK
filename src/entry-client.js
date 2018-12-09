7f320ad955b206ef9f69d95e9b10fa75414cc108
Cristian-S-Rodriguez <santillanrodriguezcristian@gmail.com>
2018-12-09 14:50:53 +0000

    client bootstrapping

---
+++ src/entry-client.js
@@ -0,0 +1,7 @@
+import { createApp } from './main.js';
+// client-specific bootstrapping logic...
+
+const { app } = createApp()
+
+//this assumes App.vue template root element has 'id="app"'
+app.$mount( '#app' )
