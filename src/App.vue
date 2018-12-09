7b510d0c5f5c02c71abca2e13c6fe4ef869786db
Cristian-S-Rodriguez <santillanrodriguezcristian@gmail.com>
2018-12-09 14:48:17 +0000

    Main app - render component

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <p>
+      <router-link :to="/">Go to Home</router-link>
+      <router-link :to="/About">Go to About</router-link>
+    </p>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+};
+</script>
